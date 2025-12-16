d9bfc2e4570f8c9db249cd06dbd916be2c714ad3
Rajesh Rajesh P <rajesh.pendem@canopusgbs.com>
2025-12-16 07:00:19 +0000

    Initial commit with AppRouter

---
+++ purchaseorderclient/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.sap.purchaseorderclient.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
